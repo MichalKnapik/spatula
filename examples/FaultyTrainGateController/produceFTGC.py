66210443a5d9b58a12f00ae9772377acf8adbd22
michal.knapik <knapikgm@gmail.com>
2014-01-17 13:06:13 +0100

    1st Formula fix

--- examples/FaultyTrainGateController/produceFTGC.py
+++ examples/FaultyTrainGateController/produceFTGC.py
@@ -7,10 +7,12 @@ if __name__ == "__main__":
         msg = """Usage: produceFTGC.py N fno
 N: number of trains, fno: formula number
 formulae:
-1 - #AG($var; ( (not (Train1in and Train2in)) or ... )) (each pair)
+1 - #AG($var; ( not ( (Train1in and Train2in) or ... )) (each pair)
     and #EF($var; Train1in) and ... (each train)
 2 - #AG($var; (#EX($var; (not Train1in or Train1in))))
-    with switches: appr1, in1, inF1, out1, outF1;"""
+    with switches: appr1, in1, inF1, out1, outF1;
+3 - #EF($var;(#AG($var; ((not Train1in) and ... and (not TrainNin))));
+"""
 
         print(msg)
         sys.exit()
@@ -85,15 +87,19 @@ module Train{0}:
     print("\nverify:")
     
     forms = []
-    mixtrin = ["(not (Train"+str(i)+"in and " + "Train"+str(j)+"in))" for i in range(1, trainsno + 1) for j in range(1, trainsno + 1) if i<j]
-    formp1 = "#AG($var; (" + "\n or ".join(mixtrin) + "))"
+
+    mixtrin = ["(Train"+str(i)+"in and " + "Train"+str(j)+"in)" for i in range(1, trainsno + 1) for j in range(1, trainsno + 1) if i<j]
+    formp1 = "#AG($var; ( not (" + "\n or ".join(mixtrin) + ")))"
     for i in range(1, trainsno + 1):
         formp1 = formp1 + "\nand #EF($var; Train"+str(i)+"in)"
     formp1 = formp1 + ";"
     forms.append(formp1)
+
     formp2 = "#AG($var; (#EX($var; (not Train1in or Train1in))));"
     formp2 = formp2 + "\nswitches: appr1,in1, inF1, out1, outF1;"
     forms.append(formp2)
 
-    print(forms[formno-1])
+    formp3 = "#EF($var;(#AG($var; (" + " and ".join(["(not Train"+str(i)+"in)" for i in range(1, trainsno + 1)]) + "))));\n"
+    forms.append(formp3)
 
+    print(forms[formno-1])
